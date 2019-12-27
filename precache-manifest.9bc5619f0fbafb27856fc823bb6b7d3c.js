bc82345d1050484f1c499f0747e7cdb64a47db47
teresita <Teresita>
2019-12-27 13:01:32 +0100

    Updates

---
+++ precache-manifest.9bc5619f0fbafb27856fc823bb6b7d3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7a5c2d4a4f3bfc2bbfda7a8509cebf0",
+    "url": "/Teresitalee/RegistroVet/index.html"
+  },
+  {
+    "revision": "e3acba594d8f1376a9a2",
+    "url": "/Teresitalee/RegistroVet/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "a969487a7e64cff41ee2",
+    "url": "/Teresitalee/RegistroVet/static/css/main.bd8cf3da.chunk.css"
+  },
+  {
+    "revision": "e3acba594d8f1376a9a2",
+    "url": "/Teresitalee/RegistroVet/static/js/2.84c66f70.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Teresitalee/RegistroVet/static/js/2.84c66f70.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a969487a7e64cff41ee2",
+    "url": "/Teresitalee/RegistroVet/static/js/main.40179d09.chunk.js"
+  },
+  {
+    "revision": "822d85926676f49ea2fa",
+    "url": "/Teresitalee/RegistroVet/static/js/runtime-main.9d65f421.js"
+  }
+]);
